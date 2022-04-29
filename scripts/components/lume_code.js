735c50c3a7458ac3d12b04bd6ad1598082080b7b
Oscar Otero <oom@oscarotero.com>
2022-04-29 17:39:35 +0200

    updated code tabs appearance

--- scripts/components/lume_code.js
+++ scripts/components/lume_code.js
@@ -9,7 +9,7 @@ export default class LumeCode extends HTMLElement {
       const li = document.createElement("li");
       const button = document.createElement("button");
       button.innerText = title;
-      button.classList.add("button", "is-secondary");
+      button.classList.add("lume-code-tab");
       button.addEventListener("click", () => {
         blocks.forEach((el) => el.hidden = el !== block);
         this.querySelectorAll("button.is-active").forEach((el) =>
