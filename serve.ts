99756fe804155ad8fb01cb102c0a54d681869a59
Oscar Otero <oom@oscarotero.com>
2022-09-09 23:31:17 +0200

    testing the fs_cache middleware to reduce the fs access

--- serve.ts
+++ serve.ts
@@ -3,18 +3,18 @@ import Server from "lume/core/server.ts";
 import expires from "lume/middlewares/expires.ts";
 import cacheBusting from "lume/middlewares/cache_busting.ts";
 import analytics from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/google_analytics/mod.ts";
+import fscache from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/fs_cache/mod.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
 
-server.use(expires());
-server.use(cacheBusting());
-server.use(analytics({
-  id: "UA-110819-22",
-}));
-
-server.start();
+server
+  .use(expires())
+  .use(cacheBusting())
+  .use(analytics({ id: "UA-110819-22" }))
+  .use(fscache())
+  .start();
 
 console.log("Listening on http://localhost:8000");
