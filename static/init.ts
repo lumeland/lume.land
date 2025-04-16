df222063a33b55238e38c531cf6c17ce16fbe0fd
Oscar Otero <oom@oscarotero.com>
2025-04-16 21:05:08 +0200

    fix init script with themes

--- static/init.ts
+++ static/init.ts
@@ -1,8 +1,13 @@
-const res = await fetch(
-  `https://cdn.deno.land/lume_init/meta/versions.json`,
-);
-const versions = await res.json();
-const { run } = await import(
-  `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
-);
-run();
+if (!Deno.args.includes("--dev") && !Deno.args.includes("-d")) {
+  const { run } = await import("https://deno.land/x/lume_init@v0.3.1/mod.ts");
+  run();
+} else {
+  const res = await fetch(
+    `https://cdn.deno.land/lume_init/meta/versions.json`,
+  );
+  const versions = await res.json();
+  const { run } = await import(
+    `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
+  );
+  run();
+}
