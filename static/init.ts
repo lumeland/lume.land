a2860ff152b5baa13ac21708e2b4e448e2f0faf8
Oscar Otero <oom@oscarotero.com>
2025-04-17 11:55:11 +0200

    Revert "fix init script with themes"
    This reverts commit df222063a33b55238e38c531cf6c17ce16fbe0fd.

--- static/init.ts
+++ static/init.ts
@@ -1,13 +1,8 @@
-if (!Deno.args.includes("--dev") && !Deno.args.includes("-d")) {
-  const { run } = await import("https://deno.land/x/lume_init@v0.3.1/mod.ts");
-  run();
-} else {
-  const res = await fetch(
-    `https://cdn.deno.land/lume_init/meta/versions.json`,
-  );
-  const versions = await res.json();
-  const { run } = await import(
-    `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
-  );
-  run();
-}
+const res = await fetch(
+  `https://cdn.deno.land/lume_init/meta/versions.json`,
+);
+const versions = await res.json();
+const { run } = await import(
+  `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
+);
+run();
