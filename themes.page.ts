1899a764029626e0db2c2fb9898342124bc0ed3b
Oscar Otero <oom@oscarotero.com>
2025-05-07 18:28:26 +0200

    show only themes compatible with lume 3

--- themes.page.ts
+++ themes.page.ts
@@ -6,6 +6,9 @@ const themes = await fetch(url).then((res) => res.json());
 
 export default async function* () {
   for (const theme of themes) {
+    if (theme.lume_version < 3) {
+      continue;
+    }
     yield {
       url: `/theme/${theme.id}/`,
       title: theme.name,
